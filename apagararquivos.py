9e540ca412932fff3c377cd828d686e0baa96f8e
MarcieL-Bezerra <marcielgestao2007.1@gmail.com>
2021-06-29 21:48:56 -0300

    Inicial

---
+++ apagararquivos.py
@@ -0,0 +1,35 @@
+import tkinter.filedialog as fdlg
+import os
+import tkinter as tk
+import tkinter.messagebox as tkMessageBox
+from tkinter import *
+import pandas as pd
+
+tinicial = tk.Tk()
+tinicial.geometry("800x500+200+100")
+tinicial.title("JUNTA ARQUIVOS - SIS")
+tinicial.resizable(width=False, height=False)
+tinicial['bg'] = '#49A'
+tinicial.iconphoto(True, PhotoImage(file='./arquivos/junta.png'))
+image=PhotoImage(file='./arquivos/junta.png')
+
+colunaum='Código da Loja'
+colunadois='CPF'
+
+
+
+caminhoinicial = './conversas'
+files = os.listdir(caminhoinicial)
+#df = pd.DataFrame()
+
+files_mp3 =[caminhoinicial + '\\' + f for f in files if f[-3:]== 'mp3']
+
+
+print(files_mp3)
+
+for f in files_mp3:
+	os.remove(f)
+
+
+
+tinicial.mainloop()
